5a6362fb6bf3f01b033643fe3a35b2f557a36c28
x40arcade <sobrepenakenneth97@gmail.com>
2026-01-02 12:15:16 +0800

    Initial project setup with User class
    Add Eclipse project files (.classpath, .project, .gitignore) and create initial User.java with basic input prompts for username and password.

---
+++ src/User.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class User {
+	public static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		String username;
+		String password;
+		
+		System.out.println("Enter Username: ");
+		username = scan.nextLine();
+		
+		System.out.println("Enter Password: ");
+	}
+
+}
